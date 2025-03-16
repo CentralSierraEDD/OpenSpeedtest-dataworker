2bb08ca3c238a6c8d312d0209917f5ff24793dfd
Trevor Ames Gregg, the Professor <64638339+gravelpeake@users.noreply.github.com>
2025-03-16 15:52:38 -0700

    t

--- src/index.ts
+++ src/index.ts
@@ -2,30 +2,21 @@ import { renderHtml } from "./renderHtml";
 
 export default {
   async fetch(request, env) {
-    let indexQuery = `SELECT * FROM data_table WHERE indexnum = (SELECT MAX(indexnum) FROM data_table);`;
+    let indexQuery = `SELECT MAX(indexnum) AS maxIndex FROM data_table;`;
     let createQuery = `INSERT INTO data_table (indexnum) VALUES (?);`;
   
-    const { sqlResult } = await env.DB.prepare(indexQuery).all();
+    const { results: indexResults } = await env.DB.prepare(indexQuery).all();
     
-    const { entries } = Object.entries(sqlResult);
-    console.log("entries: ", entries);
-
-    console.log("sqlResult: ", sqlResult);
-    console.log("sqlResult type: ", typeof sqlResult);
-
-    let currentIndex = await sqlResult;
-
-    console.log("currentIndex is: ", currentIndex);
-
+    let currentIndex = indexResults[0]?.maxIndex ?? -1;
+    console.log("Current Index:", currentIndex);
+    
     let newIndex = currentIndex + 1;
-  
-    console.log("newIndex is: ", newIndex);
+    console.log("New Index:", newIndex);
 
     await env.DB.prepare(createQuery).bind(newIndex).run();
     
     let displayQuery = `SELECT * FROM data_table LIMIT 10;`;
-    const stmt = env.DB.prepare(displayQuery);
-    const { results } = await stmt.all();
+    const { results } = await env.DB.prepare(displayQuery).all();
 
     return new Response(renderHtml(JSON.stringify(results, null, 2)), {
       headers: {
