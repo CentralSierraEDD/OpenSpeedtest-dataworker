cde20aa795fd1942cf7ee0e68468ceb1969f8f9c
Trevor Ames Gregg, the Professor <64638339+gravelpeake@users.noreply.github.com>
2025-03-16 07:50:57 -0700

    test

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,13 @@ import { renderHtml } from "./renderHtml";
 
 export default {
   async fetch(request, env) {
-    let indexQuery = `SELECT * FROM dataTable WHERE index = (SELECT MAX(index) FROM dataTable);`;
-    let createQuery = `INSERT INTO dataTable (index) VALUES (?);`;
+    let indexQuery = `SELECT * FROM data_table WHERE indexnum = (SELECT MAX(indexnum) FROM data_table);`;
+    let createQuery = `INSERT INTO data_table (indexnum) VALUES (?);`;
     let maxIndex = env.DB.prepare(indexQuery);
     let newIndex = maxIndex++;
     const stmt2 = env.DB.prepare(createQuery).bind(newIndex);
     const { results2 } = await stmt2.all();
-    const stmt = env.DB.prepare("SELECT * FROM dataTable LIMIT 10");
+    const stmt = env.DB.prepare("SELECT * FROM data_table LIMIT 10");
     const { results } = await stmt.all();
 
     return new Response(renderHtml(JSON.stringify(results, null, 2)), {
--- src/renderHtml.ts
+++ src/renderHtml.ts
@@ -2,7 +2,7 @@ export function renderHtml(content: string) {
   return `
     <!DOCTYPE html>
     <html lang="en">
-      <title>S#008: D1 page</title>
+      <title>S#009: D1 page</title>
       <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -19,7 +19,7 @@ export function renderHtml(content: string) {
         </header>
         <main>
           <p>Your D1 Database contains the following data:</p>
-          <pre><code><span style="color: #0E838F">&gt; </span>SELECT * FROM dataTable LIMIT 10;<br>${content}</code></pre>
+          <pre><code><span style="color: #0E838F">&gt; </span>SELECT * FROM data_table LIMIT 10;<br>${content}</code></pre>
           <small class="blue">
             <a target="_blank" href="https://developers.cloudflare.com/d1/tutorials/build-a-comments-api/">Build a comments API with Workers and D1</a>
           </small>
