44f39b3c3293a7eb1000c8c614c1606b5eb94e8a
Trevor Ames Gregg, the Professor <64638339+gravelpeake@users.noreply.github.com>
2025-03-16 14:44:17 -0700

    test

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,12 @@ export default {
     let indexQuery = `SELECT indexnum FROM data_table WHERE indexnum = (SELECT MAX(indexnum) FROM data_table);`;
     let createQuery = `INSERT INTO data_table (indexnum) VALUES (?);`;
   
-    let { sqlResult } = await env.DB.prepare(indexQuery).run();
-    
-    const { entries } = Object.entries(sqlResult);
+    const sqlStmt = env.DB.prepare(indexQuery).all();
+    const { sqlResult } = await sqlStmt.run();
 
+    const { entries } = Object.entries(sqlResult);
     console.log("entries: ", entries);
+
     console.log("sqlResult: ", sqlResult);
     console.log("sqlResult type: ", typeof sqlResult);
 
