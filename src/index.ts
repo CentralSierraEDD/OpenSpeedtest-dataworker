dd7099f25df7ae459a757bef390d2515da8aeba1
Trevor Ames Gregg, the Professor <64638339+gravelpeake@users.noreply.github.com>
2025-03-16 11:04:54 -0700

    major edit

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,17 @@ export default {
     let indexQuery = `SELECT indexnum FROM data_table WHERE indexnum = (SELECT MAX(indexnum) FROM data_table);`;
     let createQuery = `INSERT INTO data_table (indexnum) VALUES (?);`;
   
-    const stmt3 = await env.DB.prepare(indexQuery);
-    const { results3 } = await stmt3.all();
-  
-    console.log("results3: ", results3);
-    console.log("results3 type: ", typeof results3);
-  
-    let newIndex = await (JSON.stringify(results3) + 1);
+    const sqlResult = await env.DB.prepare(indexQuery).all();
+
+    console.log("sqlResult: ", sqlResult);
+    console.log("sqlResult type: ", typeof sqlResult);
+
+    let currentIndex = sqlResult.length > 0 ? sqlResult[0].indexnum : 0;
+    let newIndex = currentIndex + 1;
   
     console.log("newIndex is: ", newIndex);
-  
-    env.DB.prepare(createQuery).bind(newIndex);
+
+    await env.DB.prepare(createQuery).bind(newIndex).run();
     
     let displayQuery = `SELECT * FROM data_table LIMIT 10;`;
     const stmt = env.DB.prepare(displayQuery);
