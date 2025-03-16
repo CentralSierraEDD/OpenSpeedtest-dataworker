b156c2de8c651807d118d2bfdbdfd47c026a8658
Trevor Ames Gregg, the Professor <64638339+gravelpeake@users.noreply.github.com>
2025-03-16 16:34:57 -0700

    t

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,60 @@
 import { renderHtml } from "./renderHtml";
 
+const sqlFields = [
+  "testnum",
+  "entity",
+  "streetnum",
+  "street",
+  "unitapt",
+  "city",
+  "state_US",
+  "zip",
+  "firstname",
+  "lastname",
+  "email",
+  "ISP",
+  "plandown",
+  "planup",
+  "testdown",
+  "testup",
+  "testlatency",
+  "testjitter",
+  "detectedISP",
+  "detectedregion",
+  "geolocation",
+  "censusblock"
+]
+
+const dummyData = [
+  0,
+  "individual",
+  10893,
+  "Martin Terrace Ct",
+  "",
+  "Sonora",
+  "CA",
+  95370,
+  "Trevor",
+  "Gregg",
+  "Trevor@TrevorAmesGregg.com",
+  "Comcast",
+  300,
+  30,
+  278,
+  24,
+  30,
+  5,
+  "Comcast",
+  "CA",
+  "37.99606, -120.40737",
+  ""
+];
+
 export default {
   async fetch(request, env) {
     let indexQuery = `SELECT MAX(indexnum) AS maxIndex FROM data_table;`;
     let createQuery = `INSERT INTO data_table (indexnum) VALUES (?);`;
+    let recordQuery = `UPDATE data_table SET (? = ?) WHERE indexnum = ?;`;
   
     const { results: indexResults } = await env.DB.prepare(indexQuery).all();
     
@@ -15,6 +66,21 @@ export default {
 
     await env.DB.prepare(createQuery).bind(newIndex).run();
     
+    let i: number = 0;
+    while (i < 5) {
+      let curRow = 5;
+      let curField = sqlFields[i];
+      let curData = dummyData[i];
+
+      await env.DB.prepare(recordQuery).bind(curField).bind(curData).bind(curRow).run();
+
+      console.log("Iteration:", i);
+      i++;
+    }
+
+
+
+
     let displayQuery = `SELECT * FROM data_table LIMIT 10;`;
     const { results } = await env.DB.prepare(displayQuery).all();
 
