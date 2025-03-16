67b09282bb2b9be01858b4bf33b0211e787b492b
Trevor Ames Gregg, the Professor <64638339+gravelpeake@users.noreply.github.com>
2025-03-16 09:52:58 -0700

    test

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ export default {
     let createQuery = `INSERT INTO data_table (indexnum) VALUES (?);`;
     let displayQuery = `SELECT * FROM data_table LIMIT 10;`;
     
-    const stmt3 = await env.DB.prepare(indexQuery);
+    const stmt3 = env.DB.prepare(indexQuery);
     const { results3 } = await stmt3.all();
     let maxIndex = JSON.stringify(results3);
 
