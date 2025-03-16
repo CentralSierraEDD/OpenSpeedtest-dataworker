a50343adb3c0f325384f4dd3c70602a6f0c6e5a9
Trevor Ames Gregg, the Professor <64638339+gravelpeake@users.noreply.github.com>
2025-03-16 05:37:54 -0700

    yet again

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
 import { renderHtml } from "./renderHtml";
 
-let indexQuery = `SELECT * FROM user_provided_data WHERE Test_Index = (SELECT MAX(Test_Index) FROM user_provided_data)`;
-let maxIndex = DB.query(indexQuery);
-let newIndex = maxIndex++;
-let createQuery = `INSERT INTO user_provided_data (Test_Index) VALUES (?)`;
-
-
 export default {
   async fetch(request, env) {
     env.DB.query(createQuery).bind(newIndex);
+    let indexQuery = `SELECT * FROM user_provided_data WHERE Test_Index = (SELECT MAX(Test_Index) FROM user_provided_data)`;
+    let maxIndex = env.DB.query(indexQuery);
+    let newIndex = maxIndex++;
+    let createQuery = `INSERT INTO user_provided_data (Test_Index) VALUES (?)`;
     const stmt = env.DB.prepare("SELECT * FROM user_provided_data LIMIT 10");
     const { results } = await stmt.all();
 
