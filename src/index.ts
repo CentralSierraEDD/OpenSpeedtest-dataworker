8e0b4ad47139cec71010b3dbd58798137cc89b6e
Trevor Ames Gregg, the Professor <64638339+gravelpeake@users.noreply.github.com>
2025-03-16 08:04:09 -0700

    test

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import { renderHtml } from "./renderHtml";
 
 export default {
   async fetch(request, env) {
-    let indexQuery = `SELECT * FROM data_table WHERE indexnum = (SELECT MAX(indexnum) FROM data_table);`;
-    let createQuery = `INSERT INTO data_table (indexnum) VALUES (?);`;
+    let indexQuery = `"SELECT indexnum FROM data_table WHERE indexnum = (SELECT MAX(indexnum) FROM data_table);"`;
+    let createQuery = `"INSERT INTO data_table (indexnum) VALUES (?);"`;
     const stmt3 = env.DB.prepare(indexQuery);
-    const { results3 } = await stmt3.all();
+    let results3 = await stmt3.all();
     let maxIndex = results3;
     let newIndex = maxIndex++;
     const stmt2 = env.DB.prepare(createQuery).bind(newIndex);
