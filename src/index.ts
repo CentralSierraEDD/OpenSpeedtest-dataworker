c0d9c0a7b488a4e4556171e05adacf610e53437a
Trevor Ames Gregg, the Professor <64638339+gravelpeake@users.noreply.github.com>
2025-03-16 08:28:22 -0700

    var types

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,23 @@ export default {
   async fetch(request, env) {
     let indexQuery = `SELECT indexnum FROM data_table WHERE indexnum = (SELECT MAX(indexnum) FROM data_table);`;
     let createQuery = `INSERT INTO data_table (indexnum) VALUES (?);`;
-    console.log(indexQuery);
-    console.log(createQuery);
+    
+    console.log("indexing query: ", indexQuery);
+    console.log("record create query: ", createQuery);
+    
     const stmt3 = env.DB.prepare(indexQuery);
-    const { results3 } = await stmt3.all();
+    let { results3 } = await stmt3.all();
+    
     console.log("results are:", results3);
+    
     let maxIndex = results3;
+    
     console.log("maxIndex is: ", maxIndex);
+    
     let newIndex = maxIndex++;
+    
     console.log("newIndex is: ", newIndex);
+    
     const stmt2 = env.DB.prepare(createQuery).bind(newIndex);
     const { results2 } = await stmt2.all();
     const stmt = env.DB.prepare("SELECT * FROM data_table LIMIT 10");
