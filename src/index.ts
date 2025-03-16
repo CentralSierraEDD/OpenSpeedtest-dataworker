d00f2272b7d02b18c614c4fde9fae88e0e28ed4a
Trevor Ames Gregg, the Professor <64638339+gravelpeake@users.noreply.github.com>
2025-03-16 07:38:44 -0700

    semicolons

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import { renderHtml } from "./renderHtml";
 
 export default {
   async fetch(request, env) {
-    let indexQuery = `SELECT * FROM dataTable WHERE index = (SELECT MAX(index) FROM dataTable)`;
-    let createQuery = `INSERT INTO dataTable (index) VALUES (?)`;
+    let indexQuery = `SELECT * FROM dataTable WHERE index = (SELECT MAX(index) FROM dataTable);`;
+    let createQuery = `INSERT INTO dataTable (index) VALUES (?);`;
     let maxIndex = env.DB.prepare(indexQuery);
     let newIndex = maxIndex++;
     const stmt2 = env.DB.prepare(createQuery).bind(newIndex);
