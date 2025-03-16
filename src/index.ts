f0c35db058077e262669ff052f5c224ce0027980
Trevor Ames Gregg, the Professor <64638339+gravelpeake@users.noreply.github.com>
2025-03-16 10:40:56 -0700

    no async

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,24 @@
 import { renderHtml } from "./renderHtml";
 
+let indexQuery = `SELECT indexnum FROM data_table WHERE indexnum = (SELECT MAX(indexnum) FROM data_table);`;
+let createQuery = `INSERT INTO data_table (indexnum) VALUES (?);`;
+
+const stmt3 = env.DB.prepare(indexQuery);
+const { results3 } = JSON.stringify(stmt3);
+
+console.log("results3: ", results3);
+console.log("results3 type: ", typeof results3);
+
+let newIndex = JSON.stringify(results3) + 1;
+
+console.log("newIndex is: ", newIndex);
+
+const stmt2 = env.DB.prepare(createQuery).bind(newIndex);
+const { results2 } = await stmt2.all();
+
 export default {
   async fetch(request, env) {
-    let indexQuery = `SELECT indexnum FROM data_table WHERE indexnum = (SELECT MAX(indexnum) FROM data_table);`;
-    let createQuery = `INSERT INTO data_table (indexnum) VALUES (?);`;
     let displayQuery = `SELECT * FROM data_table LIMIT 10;`;
-
-    const stmt3 = env.DB.prepare(indexQuery);
-    const { results3 } = await JSON.stringify(stmt3);
-
-    console.log("results3: ", await results3);
-    console.log("results3 type: ", typeof await results3);
-
-    let newIndex = JSON.stringify(results3) + 1;
-
-    console.log("newIndex is: ", await newIndex);
-
-    const stmt2 = env.DB.prepare(createQuery).bind(newIndex);
-    const { results2 } = await stmt2.all();
-
     const stmt = env.DB.prepare(displayQuery);
     const { results } = await stmt.all();
 
