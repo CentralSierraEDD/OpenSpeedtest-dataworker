ca31d048ddebee09cab6a616f9bee2671dcefb12
Trevor Ames Gregg, the Professor <64638339+gravelpeake@users.noreply.github.com>
2025-03-27 22:16:07 -0700

    added async function and env

--- src/index.ts
+++ src/index.ts
@@ -20,74 +20,76 @@ export default class WorkflowsService extends WorkerEntrypoint {
   }
 }
 
-const sqlFields = [
-  "testnum",
-  "entity",
-  "streetnum",
-  "street",
-  "unitapt",
-  "city",
-  "state_US",
-  "zip",
-  "firstname",
-  "lastname",
-  "email",
-  "ISP",
-  "plandown",
-  "planup",
-  "testdown",
-  "testup",
-  "testlatency",
-  "testjitter",
-  "detectedISP",
-  "detectedregion",
-  "geolocation",
-  "censusblock"
-]
-var inboundData = [
-  "0",
-  "individual",
-  "streetnum",
-  "streetname",
-  "unitnum",
-  "city",
-  "state",
-  "zip",
-  "firstname",
-  "lastname",
-  "email@domain.com",
-  "reported ISP",
-  "300",
-  "30",
-  "278",
-  "24",
-  "30",
-  "5",
-  "detected ISP",
-  "geolocation",
-  "37.99606, -120.40737",
-  "censusdata"
-];
-const sqlFieldsNum = sqlFields.length;
-const requestData = payload;
-console.log('Received data:', requestData);
+async function addData(env:any, payload) {
+  const sqlFields = [
+    "testnum",
+    "entity",
+    "streetnum",
+    "street",
+    "unitapt",
+    "city",
+    "state_US",
+    "zip",
+    "firstname",
+    "lastname",
+    "email",
+    "ISP",
+    "plandown",
+    "planup",
+    "testdown",
+    "testup",
+    "testlatency",
+    "testjitter",
+    "detectedISP",
+    "detectedregion",
+    "geolocation",
+    "censusblock"
+  ]
+  var inboundData = [
+    "0",
+    "individual",
+    "streetnum",
+    "streetname",
+    "unitnum",
+    "city",
+    "state",
+    "zip",
+    "firstname",
+    "lastname",
+    "email@domain.com",
+    "reported ISP",
+    "300",
+    "30",
+    "278",
+    "24",
+    "30",
+    "5",
+    "detected ISP",
+    "geolocation",
+    "37.99606, -120.40737",
+    "censusdata"
+  ];
+  const sqlFieldsNum = sqlFields.length;
+  const requestData = payload;
+  console.log('Received data:', requestData);
 
-var inboundData = requestData;
-let indexQuery = `SELECT MAX(indexnum) AS maxIndex FROM data_table;`;
-let createQuery = `INSERT INTO data_table (indexnum) VALUES (?);`;
+  var inboundData = requestData;
+  let indexQuery = `SELECT MAX(indexnum) AS maxIndex FROM data_table;`;
+  let createQuery = `INSERT INTO data_table (indexnum) VALUES (?);`;
 
-const { results: indexResults } = await env.DB.prepare(indexQuery).all();
-let currentIndex = indexResults[0]?.maxIndex ?? -1;
-let newIndex = currentIndex + 1;
-console.log("Current Index:", currentIndex);
-console.log("New Index:", newIndex);
+  const { results: indexResults } = await env.DB.prepare(indexQuery).all();
+  let currentIndex = indexResults[0]?.maxIndex ?? -1;
+  let newIndex = currentIndex + 1;
+  console.log("Current Index:", currentIndex);
+  console.log("New Index:", newIndex);
 
-await env.DB.prepare(createQuery).bind(newIndex).run();
+  await env.DB.prepare(createQuery).bind(newIndex).run();
 
-for (let i = 0; i < sqlFieldsNum; i++) {
-  let curRow = newIndex;
-  let curField = sqlFields[i];
-  let curData = inboundData[i];
-  await env.DB.prepare(`UPDATE data_table SET ${curField} = ? WHERE indexnum = ?;`).bind(curData, curRow).run();
-  console.log("Iteration:", i, " , Row:", curRow, " , Field:", curField, " , Data:", curData);
-}
+  for (let i = 0; i < sqlFieldsNum; i++) {
+    let curRow = newIndex;
+    let curField = sqlFields[i];
+    let curData = inboundData[i];
+    await env.DB.prepare(`UPDATE data_table SET ${curField} = ? WHERE indexnum = ?;`).bind(curData, curRow).run();
+    console.log("Iteration:", i, " , Row:", curRow, " , Field:", curField, " , Data:", curData);
+  };
+};
