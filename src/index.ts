43931a43a74fefc70b7e1d4d103e782569bcbea0
Trevor Ames Gregg, the Professor <64638339+gravelpeake@users.noreply.github.com>
2025-03-27 22:13:41 -0700

    t

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ var payload = "";
 
 export default class WorkflowsService extends WorkerEntrypoint {
   // Currently, entrypoints without a named handler are not supported
-  async fetch(env: any) {
+  async fetch(request, env) {
     console.log("env:", env);
     return new Response(null, { status: 404 });
   }
